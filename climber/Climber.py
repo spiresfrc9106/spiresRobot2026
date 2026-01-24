f2a5b66f4da329cc10b4a9f7de2fac5861cc29ff
Q-Bert <frc1736student@gmail.com>
2026-01-24 09:58:51 -0600

    links CANIDs

--- climber/Climber.py
+++ climber/Climber.py
@@ -2,24 +2,32 @@
 import math
 from rev import SparkMax, SparkMaxConfig, SparkBaseConfig
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
+from utils.constants import CLIMBER_ARM_CANID, CLIMBER_PIVOT_CANID
 class Climber:
     def Init(self):
         """Runs once when the robot starts."""
-        self.arm_motor = WrapperedSparkMax(1, "ArmMotor", brakeMode=True)
-        self.pivot_motor = WrapperedSparkMax(2, "PivotMotor", brakeMode=True)
+        self.arm_motor = WrapperedSparkMax(CLIMBER_ARM_CANID, "ArmMotor", brakeMode=True)
+        self.pivot_motor = WrapperedSparkMax(CLIMBER_PIVOT_CANID, "PivotMotor", brakeMode=True)
         self.timer = 0
         self.is_moving = False
         self.move_duration = 0
         self.speed = 0
+        self.distance = 0
     def setSpeed(self, newSpeed):
         self.speed = newSpeed
 
     def getSpeed(self):
         return self.speed
-
-    def startClimb(self,distance):
-        if self.arm_motor.getMotorPositionRad() < distance:
+    def getDistance(self):
+        return self.distance
+   
+    def setDistance(self, newDistance):
+        self.distance = newDistance
+    def startClimb(self):
+        if (self.arm_motor.getMotorPositionRad() < self.distance and self.speed >(0)) \
+        or (self.arm_motor.getMotorPositionRad() > self.distance and self.speed <(0)):
+        
             self.pivot_motor.setPosCmd(0)
                  #TODO: retract intake.
-            self.arm_motor.setVelCmd(6)
+            self.arm_motor.setVelCmd(0)
             self.arm_motor.getMotorPositionRad()
