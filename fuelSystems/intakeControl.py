d6012f2ce1fd1fa4c7e2886b774671e3d5ef42e3
Q-Bert <frc1736student@gmail.com>
2026-01-31 11:06:43 -0600

    base for intakeControl.py

--- fuelSystems/intakeControl.py
+++ fuelSystems/intakeControl.py
@@ -1,21 +1,33 @@
 from utils.singleton import Singleton
+from utils.constants import INTAKE_CONTROL_CANID, INTAKE_WHEELS_CANID
+from wrappers.wrapperedSparkMax import WrapperedSparkMax
 
 class IntakeController(metaclass=Singleton):
 
     def __init__(self):
 
         self.intakeEnabled = False
-
+        self.intakeMotor = WrapperedSparkMax("intake_Motor",INTAKE_CONTROL_CANID)
+        self.intakeWheelsMotor = WrapperedSparkMax("intake_Wheels_Motor",INTAKE_WHEELS_CANID)
         pass
 
     def update(self):
         pass 
 
     def enableIntake(self):
-        pass 
+        self.intakeEnabled = True
+        self.intakeWheelsMotor.setVelCmd(8)
 
     def disableIntake(self):
-        pass 
-
+        self.intakeEnabled = False
+        self.intakeWheelsMotor.setVelCmd(0)
     def getIntakeState(self):
         return self.intakeEnabled 
+    
+    def lowerIntake(self):
+        self.intakeMotor.setPosCmd(8)
+        
+    def raiseIntake(self):
+        self.intakeMotor.setPosCmd(0)
+       
+    
